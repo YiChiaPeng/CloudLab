878c8ad8ce9338bdc5914c6e387ef5dbbd00351c
Xuemao1018 <84667037+Xuemao1018@users.noreply.github.com>
2022-08-24 18:21:10 +0800

    again

--- server/api/start.py
+++ server/api/start.py
@@ -2,7 +2,7 @@
 # 載入Flask套件
 from flask import Flask,jsonify
 from flask_restful import Api, Resource
-
+from flask import render_template
 
 '''--------------------------------------
     import api所提供的resouce file
@@ -26,5 +26,8 @@ api.add_resource(test, "/")
 api.add_resource(User, "/User/<string:id>")
 api.add_resource(Users, "/Users")
 api.add_resource(login_handler,"/api/login")
+@app.route("/login")
+def getLogin():
+    return render_template("Logging.html")
 if __name__ == "__main__":
     app.run(port=8087,debug=True)
