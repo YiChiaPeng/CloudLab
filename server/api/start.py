c88c48f471e65836c3fcd2e4f8c89c8693ebece7
k34425707 <b102145@gmail.com>
2022-08-24 17:20:34 +0800

    update start.py

--- server/api/start.py
+++ server/api/start.py
@@ -1,9 +1,9 @@
 #!/bin/python3
 # 載入Flask套件
-from flask import Flask,jsonify
-from flask_restful import Api, Resource
-
-
+from flask import Flask,jsonify, render_template
+from flask_restful import Api
+from common.JWT_handler import JWT_handler
+from flask_jwt_extended import  JWTManager,jwt_required
 '''--------------------------------------
     import api所提供的resouce file
 ---------------------------------------'''
@@ -13,6 +13,9 @@ from resource.Users import Users
 from resource.login_handler import login_handler
 
 app = Flask(__name__)
+jwt=JWTManager(app)
+app.config['JWT_SECRET_KEY'] = 'test' 
+
 '''
 加一個驗證
 '''
@@ -21,10 +24,13 @@ app = Flask(__name__)
 '''
 Api要提供的resource放在resource
 '''
+
+
 api = Api(app)
 api.add_resource(test, "/")
 api.add_resource(User, "/User/<string:id>")
 api.add_resource(Users, "/Users")
 api.add_resource(login_handler,"/api/login")
+
 if __name__ == "__main__":
     app.run(port=8087,debug=True)
