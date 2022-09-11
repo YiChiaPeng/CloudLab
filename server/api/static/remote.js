26078c3fed72946c7f661cbbc067e728e982539b
k34425707 <b102145@gmail.com>
2022-09-11 19:08:20 +0800

    fix

--- server/api/static/remote.js
+++ server/api/static/remote.js
@@ -27,7 +27,9 @@ function do_pay() {
     formData.append('sofFile', document.getElementById('soffile').files[0]);
     formData.append('pgvFile', document.getElementById('pgvfile').files[0]);
     formData.append('workType', 0);
-
+    document.getElementById("law").innerHTML="<button class='btn btn-primary' style='color: black; background-color: white; border-color: white;' type='button' disabled><span class='spinner-border spinner-border-sm' style='color: black;' role='status' aria-hidden='true'></span> 燒錄中...</button>"
+    document.getElementById("sof").innerHTML="<th><a href='/getfile/sof' download>sof載點</a></th>"
+    document.getElementById("pgv").innerHTML="<th><a href='/getfile/pgv' download>pgv載點</a></th>"
     $.ajax({
         type: 'POST',
         url: '/api/ProgrammingRequest',
@@ -37,6 +39,51 @@ function do_pay() {
         data: formData,
         processData: false,
         contentType: false,
-        success: function(response){}
+        success: function(response) {
+            start()
+        }
     })
+}
+
+function start(){
+    console.log("start() line1")
+    $.ajax({
+        method: 'GET',
+        url:"/remote/getStatus",
+        headers: {
+            'Authorization': 'Bearer ' + currentCookie
+        },
+        success: function(response){
+            console.log(response);
+            document.getElementById("time").innerHTML="<th>"+response.time+"</th>"
+            if(response.success=="t" && response.status==0){
+                console.log("interval");
+                window.setInterval("renewLAW();",5000);
+            }else{
+                document.getElementById("law").innerHTML="<a href='/getfile/law' download>law檔載點</a>"
+
+            }
+        }
+    });
+}
+
+function renewLAW(){
+    $.ajax({
+        method: 'GET',
+        url:"/remote/getStatus",
+        headers: {
+            'Authorization': 'Bearer ' + currentCookie
+        },
+        success: function(response){
+            if(response.success=="t" && response.status=="0"){
+                document.getElementById("law").innerHTML="<button class='btn btn-primary' style='color: black; background-color: white; border-color: white;' type='button' disabled><span class='spinner-border spinner-border-sm' style='color: black;' role='status' aria-hidden='true'></span> 燒錄中...</button>"
+            }else{
+                document.getElementById("law").innerHTML="<a href='/getfile/law' download>law檔載點</a>"
+            }
+        }
+    });
+}
+
+window.onload=function(){
+    start()
 }
